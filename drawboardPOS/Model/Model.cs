bfd3546c144ca497594fa511b0f3057895edc974
Srinivas Teella <srinivas.teella@helloworld.com.au>
2019-02-23 18:51:02 +1100

    initial version

---
+++ drawboardPOS/Model/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace drawboardPOS.Model
+{
+
+    public class VolumePrice
+    {
+        public int Volume { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double OriginalPrice { get; set; }
+        public VolumePrice VolumePrice { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Product> Products { get; set; }
+    }
+
+
+}
---
+++ drawboardPOS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace drawboardPOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PointOfSaleTerminal a = new PointOfSaleTerminal();
+            string[] productarray = new string[] { "Donut", "KitKat", "Donut", "Lolly", "Cheese", "Donut", "KitKat", "Donut", "Donut", "Donut", "Donut" };
+            var groupedProdList = groupby(productarray);
+            double totalPrice = 0;
+            foreach (var product in groupedProdList)
+            {
+                var prod = a.Scan(product);
+                a.CalculateTotal(prod, product, ref totalPrice);
+            }
+            Console.WriteLine("Total Price:" + totalPrice);
+            Console.Read();
+        }
+
+        private static dynamic groupby(string[] productarray)
+        {
+            var output = productarray
+                .GroupBy(product => product)
+                .OrderByDescending(group => group.Count())
+                .Select(group => new { productname = group.Key, count = group.Count() });
+
+            return output;
+        }
+    }
+}
